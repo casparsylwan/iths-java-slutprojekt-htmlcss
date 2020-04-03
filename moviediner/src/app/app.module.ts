9fa147d369a5732962ddd68a83f9bb439fe6025a
casparsylwan <casparsylwan@gmail.com>
2020-04-03 13:45:56 +0200

    topnav mobile size

--- moviediner/src/app/app.module.ts
+++ moviediner/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PartsComponent } from './components/parts/parts.component';
+import { FrontComponent } from './components/pages/front/front.component';
+import { TopnavComponent } from './components/topnav/topnav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PartsComponent,
+    FrontComponent,
+    TopnavComponent
   ],
   imports: [
     BrowserModule,
